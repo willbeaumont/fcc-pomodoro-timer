f6eadcada9610a4d8d452d78960684165d7eb9fe
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-10-25 19:30:54 -0700

    user story 3 and 4

--- src/App.js
+++ src/App.js
@@ -9,9 +9,15 @@ function App() {
       <body>
         <div>
           <label id="break-label">Break Length</label>
+          <button id="break-increment" class="adjustTimer">&uarr;</button>
+          <div id="break-length"></div>
+          <button id="break-decrement" class="adjustTimer">&darr;</button>
         </div>
         <div>
           <label id="session-label">Session Length</label>
+          <button id="session-increment" class="adjustTimer">&uarr;</button>
+          <div id="session-length"></div>
+          <button id="session-decrement" class="adjustTimer">&darr;</button>
         </div>
       </body>
       <footer>
